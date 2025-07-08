e2908e1ee9db358659f1e1f20f098b5250dd821d
jeromecagado <jerome.cagado@outlook.com>
2025-07-08 14:03:54 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Assignments1._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("We will check if two integers are equal.");
+            Console.WriteLine("Please enter the first number");
+            int fistIntegerNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the second number");
+            int secondIntegerNumber = Convert.ToInt32(Console.ReadLine());
+            if (fistIntegerNumber != secondIntegerNumber)
+            {
+                Console.WriteLine("They are not equal!");
+            } else
+            {
+                Console.WriteLine("They are equal!");
+            }
+            Console.Read();
+        }
+    }
+}
